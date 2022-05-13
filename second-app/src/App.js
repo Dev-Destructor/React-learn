a68c476cbfb2b5f98285b34b2ecd1036e14605e3
Destructor007 <des578435@gmail.com>
2022-05-13 23:41:22 +0530

    added data fetching with axios

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 
 export default class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {})
+  state = { images: [] };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ZSudKz2Dw8PGg8yI45Gc8gNtLaObSUmTGz8fCvoYFdw'
+      }
+    })
+    this.setState({ images: response.data.results })
   }
   
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: { this.state.images.length } images
       </div>
     )
   }
