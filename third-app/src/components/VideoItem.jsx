aa82d993bdf40b67661cd537747517f2ec2b7f29
Destructor007 <des578435@gmail.com>
2022-05-16 12:17:13 +0530

    added video thumbnail and title

--- third-app/src/components/VideoItem.jsx
+++ third-app/src/components/VideoItem.jsx
@@ -1,17 +1,19 @@
-import './css/VideoItem.css'
+import "./css/VideoItem.css";
 import React from "react";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
-      <img src={video.snippet.thumbnails.high.url} alt={video.snippet.title} className="ui image" />
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img
+        src={video.snippet.thumbnails.high.url}
+        alt={video.snippet.title}
+        className="ui image"
+      />
       <div className="content">
-        <div className="header">
-          {video.snippet.title}
-        </div>
+        <div className="header">{video.snippet.title}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default VideoItem;
